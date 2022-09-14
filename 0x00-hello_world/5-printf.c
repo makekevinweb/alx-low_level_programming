9bdb379dd1ba9debc9d82634126a48accc1ed6b4
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-14 14:57:43 -0700

    debug

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ *main -Entry point
+ *This is is a printf function
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
