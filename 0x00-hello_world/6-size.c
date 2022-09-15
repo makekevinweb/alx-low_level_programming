774e45c3023ba8fd01acaf6e88a5b7338ae92c6b
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-15 07:06:48 -0700

    debug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
