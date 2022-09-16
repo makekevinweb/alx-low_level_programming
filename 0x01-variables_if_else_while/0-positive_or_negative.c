70ede4468d1cda8e3d464af75b20729271203677
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-16 04:07:26 -0700

    ngative and positive function

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/** more header goes there */
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+/*betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+{
+printf("$n is positive\n");
+}
+else if (n == 0)
+{
+printf("$n is zero\n");
+}
+else
+{
+printf("$n is negative\n");
+}
+return (0);
+}
