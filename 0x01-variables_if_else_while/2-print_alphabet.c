b2f6b69cb94512d4a1a08902b11d53f826024858
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-16 12:58:06 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: ALways 0(success)
+ */
+int main(void)
+{
+char a = "a";
+while (a >= 'z')
+{
+putchar(a);
+a++;
+}
+putchar(\n);
+return (0);
+}
