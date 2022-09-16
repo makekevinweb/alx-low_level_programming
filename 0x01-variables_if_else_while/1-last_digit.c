c7c3fed5389e53217a6b9befed31e8778d558858
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-16 11:20:29 -0700

    debugg

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ ln = n % 10;
 printf("Last digit of %d is %d", n, ln);
 if (ln > 5)
 {
-printf("and is greater than 5");
+printf(" and is greater than 5\n");
 }
 else if (ln == 0)
 {
-printf("and is 0");
+printf(" and is 0\n");
 }
 else
 {
-printf("and is less than 6 and not 0 \n");
+printf(" and is less than 6 and not 0 \n");
 }
 return (0);
 }
