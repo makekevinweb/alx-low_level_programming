7ccaf3e7ec34cce26bf53cebcc94722813c40e2e
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-16 18:54:44 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int a = 0;
+  while (a <= 9)
+    {
+      printf(a);
+      a++;
+    }
+  printf('\n');
+  return (0);
+}
