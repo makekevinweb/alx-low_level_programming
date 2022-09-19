87bc629837b185268b8b1fc7559cdbb2338653c6
Marius Kevin IKUZWE <ikuzwemariuskevin@gmail.com>
2022-09-19 21:42:50 +0000

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,7 @@
  * @str: this is container of the string
  *return: The string to be printed
  */
-void puts(char *str)
+void _puts(char *str)
 {
 int index;
 for (index = 0; str[index] != '\0'; index++)
