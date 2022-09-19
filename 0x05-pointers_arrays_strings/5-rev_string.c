ea2902e7243b87c7010dadbf111d3ab6b157dd6f
Marius Kevin IKUZWE <ikuzwemariuskevin@gmail.com>
2022-09-19 22:19:51 +0000

    task 5 paused

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *rev-string -function that reverses a string
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+  int index;
+  for(index = 0; s[index])
+}
