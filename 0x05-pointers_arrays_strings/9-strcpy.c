dbc36a7afccd5d4391850d435d8cf984ab8a2e34
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-09-20 12:18:53 +0000

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ **_strcpy -copy the string to another
+ *@dest: the pointer to the array to copy to
+ *@src: the pointer of the string to copy
+ *Return: Always pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+while (*src != '\0')
+{
+*dest = *src;
+*dest++;
+*src++;
+}
+*dest = '\0';
+return (dest);
+}
