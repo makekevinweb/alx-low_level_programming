089fefa201c9de7dfb973f4e8c6d575dc903185e
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-09-20 10:30:26 +0000

    cdebug

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,15 @@
 void puts2(char *str)
 {
 int i;
+int j;
 i = 0;
 while (str[i] != '0')
 {
-if (i % 2 == 0)
-{
-_putchar(str[i]);
-}
 i++;
 }
+for (j = 0; j < i; j = j + 2)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
 }
