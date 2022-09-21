d55c526c65ad356f77b117093b6b281f086a4280
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-09-21 04:36:47 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_islower -function to check lower case character
+ *@c: the charcacter to be checked
+ *return:1 for lowercase characters and 0 for anytthing else
+ */
+int _islower(int c);
+{
+int c;
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
