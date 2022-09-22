b1b33a1678654a6d24db2dd9fb00611eac0fc12c
keycodvin <ikuzwemariuskevin@gmail.com>
2022-09-22 02:39:40 +0000

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *jack_bauer -function to print time
+ * h = hour, m= minutes
+ *return: void
+ */
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60 ; m++)
+{
+_putchar((h / 10));
+_putchar((h % 10));
+_putchar(':');
+_putchar((m / 10));
+_putchar((m % 10));
+_putchar('\n');
+}
+}
+}
