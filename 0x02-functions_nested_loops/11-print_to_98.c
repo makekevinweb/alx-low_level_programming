24648439543c84829c73721bd3534232039722be
keycodvin <ikuzwemariuskevin@gmail.com>
2022-09-22 07:46:17 +0000

    debug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,28 @@
  */
 void print_to_98(int n)
 {
+if (n < 98)
+{
 while (n < 98)
 {
 printf("%d", n);
-_putchar(',');
-_putchar(' ');
+if (n != 98)
+printf(", ");
 n++;
 }
+}
+else if (n > 98)
+{
+while (n > 98)
+{
 printf("%d", n);
-_putchar('\n');
+if (n != 98)
+printf(", ");
+n--;
+}
+}
+else
+{
+printf("98");
+}
 }
