f6ade75d262a51231cc0098440f02fac61b7750f
keycodvin <ikuzwemariuskevin@gmail.com>
2022-09-22 03:16:38 +0000

    still ongoing work

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *times_table -fucntion to print the 9 timetables
+ *rone = row, cone = column, d = digits of current result
+ *Return: timetables
+ *add extra space past single digit
+ */
+void times_table(void)
+{
+  int rone, cone, d;
+  
+}
