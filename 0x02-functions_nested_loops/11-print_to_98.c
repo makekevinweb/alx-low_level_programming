3c67b53e8b53ace614bbd7c1eae48b3148fd16a1
keycodvin <ikuzwemariuskevin@gmail.com>
2022-09-22 06:05:53 +0000

    debug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_to_98 -functionn to print numbers in order up to 98
  *@n: number to start from
@@ -7,11 +8,11 @@ void print_to_98(int n)
 {
 while (n < 98)
 {
-_putchar(n);
+prtinf(n);
 _putchar(',');
 _putchar(' ');
 n++;
 }
-_putchar(n);
+printf(n);
 _putchar('\n');
 }
