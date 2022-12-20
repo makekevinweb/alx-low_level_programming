aff3d9299fb6b4bd7c0623e79ea927e9ab063333
Marius Kevin IKUZWE <ikuzwemariuskevin7@gmail.com>
2022-12-20 03:06:10 -0800

    a code to se the last number of a random number is created

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0(Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("The last digit of")
+if (n > 5)
+{
+printf("%d is greater than 5", n);
+}
+else if (n == 0)
+{
+printf("%d is 0", n);
+}
+else
+{
+printf("%d is less than 6 and not 0", n);
+}
