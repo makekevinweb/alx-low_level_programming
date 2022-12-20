843d2237bd104819f2147789d766b62da25238f4
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-12-20 15:10:28 +0000

    a code using putchar to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: ALways 0(success)
+ */
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
