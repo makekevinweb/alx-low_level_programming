82395d6574ff8277b49293f0a1b4a9ab81c65c9d
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-12-22 19:28:15 +0000

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,5 @@
+6-cap_string.c
+
 #include "main.h"
 
 /**
@@ -8,25 +10,26 @@
  */
 char *cap_string(char *str)
 {
-int index = 0;
-while (str[index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
+  int index = 0;
+
+  while (str[index])
+    {
+      while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
 
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index -
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
+      if (str[index - 1] == ' ' ||
+	      str[index - 1] == '\t' ||
+	      str[index - 1] == '\n' ||
+	  str[index - 1] == ',' ||
+	  str[index - 1] == ';' ||
+	      str[index - 1] == '.' ||
+	      str[index - 1] == '!' ||
+	      str[index - 1] == '?' ||
+	      str[index - 1] == '"' ||
+	  str[index - 1] == '(' ||
+	  str[index - 1] == ')' ||
+	  str[index - 1] == '{' ||
+	  str[index - 1] == '}' ||
 index == 0)
 str[index] -= 32;
 
