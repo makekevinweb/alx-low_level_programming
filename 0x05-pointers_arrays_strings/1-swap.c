3b063b35d476805a238e8e980251a46af85699b8
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-12-22 17:18:48 +0000

    lets swap numbers together

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ *swap_int- this is a functin to swap two numbers together
+ *
+ *@a: this is the first number
+ *@b: this is the second number
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+int d;
+c = a;
+d = b;
+a = d;
+b = c;
+}
