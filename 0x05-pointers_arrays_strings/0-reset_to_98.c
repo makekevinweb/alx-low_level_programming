4ce4705350f1c89c8e3645fdb93389aa3c9e2976
keycodvin <ikuzwemariuskevin7@gmail.com>
2022-12-22 17:00:01 +0000

    m

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
 /**
- *resert-to_98 - the integer to be changed is put in here
+ *reset_to_98 - the integer to be changed is put in here
  *
  *Return: Always 0(Success)
+ *
  */
+
 void reset_to_98(int *n)
 {
-char n;
-char *p;
-*p = n;
-p = &n;
-n = 402;
-putchar(n);
-*p = 98;
-putchar(n);
+*n = 98
 }
