bf90c2de0fca23a1783ba8d56181434dfe2ef4e3
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-05 07:16:34 +0000

    full revise again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,30 @@
 #include "main.h"
 #include <stddef.h>
 /**
-*_strchr - the function searches for a certain string in a character
-*
-*@s: this is a pointer to the string s
-*@c: this is the character we are searching for
-*
-*Return: pointer to the first occurence of c Or NULL if the c is not found
-*/
+ *_strchr - the function searches for a certain string in a character
+ *
+ *@s: this is a pointer to the string s
+ *@c: this is the character we are searching for
+ *
+ *Return: pointer to the first occurence of c Or NULL if the c is not found
+ */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; *s != '\0'; i++)/* go through the string up to null character*/
+while (*s != '\0')/* go through the string until we reach the null character*/
 {
-if (c == s[i])
+if (c == *s)/* check if current character is c*/
 {
-return (s + i);/* return a pointer to current character if it is c*/
+return (s);/* return a pointer to current character if it is c*/
 }
+s++; /*move to the next character in the string*/
+}
+if (*s == '\0')/*this solves issue of handling \0*/
+{
+return (s);
+}
+else
+{
+return (NULL);
 }
 return (NULL);/* in case of C not found in the string returns NULL*/
 }
