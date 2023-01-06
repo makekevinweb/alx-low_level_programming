6d4c41c70fad8b9c324c7f1e8b0de3b1aa6e9a60
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-06 13:01:23 +0000

    debugg betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,21 +14,20 @@ for (j = 0; j <= n; j++)
 {
 k = j * i;/* k is the product of i and j which is to be printed*/
 
-/* this run for product under 10, when it goes beyond 10 we must use printf
- * but in order to continue using putchar we will add in more code
- */
- if (k == 0)
-   {
-     _putchar(k + '0');
-   }
- /* this statement is for the first line of o's */
- if ( k == 0 && i == 0 && j != n)
-   {
-     _putchar(',');
-     _putchar(' ');
-     _putchar(' ');
-     _putchar(' ');
-   }
+/* this pritns the first 0's zeros in all lines*/
+if (k == 0)
+{
+_putchar(k + '0');
+}
+/* this statement is for the first line of 0's */
+if (k == 0 && i == 0 && j != n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+/* prints for value between 0 and 9*/
 if (k < 10 && k != 0)
 {
 _putchar(',');
@@ -44,19 +43,19 @@ else if (k > 9 && k < 100)
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
- _putchar((k / 10) + '0');/* this prints the first digit of the double digit number*/
- _putchar((k % 10) + '0');/* this prints the last digit of the double digit number*/
+_putchar((k / 10) + '0');/* this prints the first digit of the double digit number*/
+_putchar((k % 10) + '0');/* this prints the last digit of the double digit number*/
 }
 /* the following prints values between 99 and 999*/
- else if (k > 99 && k < 1000)
-   {
-	 _putchar(',');
-	 _putchar(' ');/* we use one space in order to make sure table is uniform and look goods*/
-	 _putchar((k / 100) + '0');/* this prints the first digit of the double digit number*/
-	 _putchar(((k % 100)/10) + '0');/* this prints the second digit of the double digit number*/
-	 _putchar((k % 10) + '0');/* this always prints the last digit*/
-          }
- }
+else if (k > 99 && k < 1000)
+{
+_putchar(',');
+_putchar(' ');/* we use one space in order to make sure table is uniform and look goods*/
+_putchar((k / 100) + '0');/* this prints the first digit of the double digit number*/
+_putchar(((k % 100) / 10) + '0');/* this prints the second digit of the double digit number*/
+_putchar((k % 10) + '0');/* this always prints the last digit*/
+}
+}
 _putchar('\n');/*add a new line afer first row of multiplication*/
 }
 }
