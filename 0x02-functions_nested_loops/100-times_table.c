1206d177e22ca259829947a9033bc1abd86ae6e9
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-06 12:34:16 +0000

    debugg

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,44 +17,37 @@ k = j * i;/* k is the product of i and j which is to be printed*/
 /* this run for product under 10, when it goes beyond 10 we must use printf
  * but in order to continue using putchar we will add in more code
  */
-if (k < 10)
-{
-_putchar(k + '0');
- 
-if (j < n) /* it adds comma and space between for the multiplication table*/
+ if (k = 0)
+   {
+     _putchar(k + '0');
+   }
+if (k < 10 && k != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
-} 
+_putchar(k + '0');
+}
 }
 /*this prints values between 9 and 100*/
 else if (k > 9 && k < 100)
 {
-_putchar((k / 10) + '0');/* this prints the first digit of the double digit number*/
-_putchar((k % 10) + '0');/* this prints the last digit of the double digit number*/
-
-if (j < n) /* it adds comma and space between for the multiplication table*/
-{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-}
+ _putchar((k / 10) + '0');/* this prints the first digit of the double digit number*/
+ _putchar((k % 10) + '0');/* this prints the last digit of the double digit number*/
 }
 /* the following prints values between 99 and 999*/
  else if (k > 99 && k < 1000)
    {
-     _putchar((k / 100) + '0');/* this prints the first digit of the double digit number*/
-     _putchar(((k % 100)/10) + '0');/* this prints the second digit of the double digit number*/
-     _putchar((k % 10) + '0');/* this always prints the last digit*/
-     
-     if (j < n) /* it adds comma and space between for the multiplication table*/
-       {
 	 _putchar(',');
 	 _putchar(' ');/* we use one space in order to make sure table is uniform and look goods*/
-       }
-   }
+	 _putchar((k / 100) + '0');/* this prints the first digit of the double digit number*/
+	 _putchar(((k % 100)/10) + '0');/* this prints the second digit of the double digit number*/
+	 _putchar((k % 10) + '0');/* this always prints the last digit*/
+          }
  }
 _putchar('\n');/*add a new line afer first row of multiplication*/
 }
