faf3b9a8b6789ab723267e8ec1726222e52dc0c5
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-06 06:49:10 +0000

    betty style correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,19 +8,20 @@ void print_times_table(int n)
 int i, j, k;
 if (n >= 0 && n <= 15)/*check if n is btn 0 and 15*/
 {
-  for (i = 0; i <= n; i++)
-    {
-      for (j = 0; j <= n; j++)
-	{
-	  k = j * i;
-	  _putchar(k + '0');
-	  if (j < n)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-	}
-      _putchar('\n');
-    }
- }
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = j * i;/* k is the product of i and j which is to be printed*/
+_putchar(k + '0');
+if (j < n) /* it adds comma and space between for the multiplication table*/
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+_putchar('\n');/*add a new line afer first row of multiplication*/
+}
+}
 }
