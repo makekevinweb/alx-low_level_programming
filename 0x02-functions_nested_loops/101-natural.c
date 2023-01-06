f33da79a91a1efd5e88a9c0a121a7bc75c3504f2
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-06 18:55:36 +0000

    debugg

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,26 +8,26 @@
  */
 int main (void)
 {
-  int sum, sum3, sum5;
-  int i;
-  sum = 0;
-  sum3 = 0;
-  sum5 = 0;
-  for (i = 0; i < 1024; i++)
-    {
-     
-      if ((i % 3) == 0)
-	{
-	  sum3 = sum3 + i;
-	 
-	}
-      else if ((i % 5) == 0)
-	{
-	  sum5 = sum5 + i;
+int sum, sum3, sum5;
+int i;
+sum = 0;
+sum3 = 0;
+sum5 = 0;
+for (i = 0; i < 1024; i++)
+{
+ 
+if ((i % 3) == 0)
+{
+sum3 = sum3 + i;
+ 
+}
+else if ((i % 5) == 0)
+{
+sum5 = sum5 + i;
 
-	}
-    }
-  sum = sum3 + sum5;
-  printf("%d \n", sum);
-  return (0);
+}
+}
+sum = sum3 + sum5;
+printf("%d\n", sum);
+return (0);
 } 
