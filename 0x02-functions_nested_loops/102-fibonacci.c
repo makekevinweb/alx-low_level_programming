6bb4b677d75722041ae9e60e1dd9565d07bf1582
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-06 21:12:12 +0000

    debugg

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,24 +6,25 @@
  */
 int main(void)
 {
-  int n; /* the fibonacci number to calculate*/ 
- long int i, j, next;/* numbers to aid as in calculation; we use long int because they hold bigger values
-		      because if we don't the statemt for i != 12586269025* we always be true*/
-  i = 1;
-  j = 2;
-  for (n = 1; n <= 50; n++)
-    {
-      if (i != 20365011074)
-	{
-	  printf("%ld, ", i);
-	}
-      else
-	{
-	  printf("%ld\n", i);
-	}
-      next = i + j;
-      i = j;
-      j = next;
-    }
-  return (0);
+int n; /* the fibonacci number to calculate*/
+long int i, j, next;
+/*numbers used in calculation; long int is used as it can hold bigger values*/
+/*because if we don't the statemt for i != 12586269025* we always be true*/
+i = 1;
+j = 2;
+for (n = 1; n <= 50; n++)
+{
+if (i != 20365011074)
+{
+printf("%ld, ", i);
+}
+else
+{
+printf("%ld\n", i);
+}
+next = i + j;
+i = j;
+j = next;
+}
+return (0);
 }
