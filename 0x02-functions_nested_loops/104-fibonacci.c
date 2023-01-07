60c4b298c3fcc1b79ab13d6d0c738716dc8cc74c
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-07 18:39:36 +0000

    new file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main -  prints the fibonacci sequence up to F(50)
+ *
+ *Return: always 0 on sucess
+ */
+int main(void)
+{
+  int n; /* the fibonacci number to calculate*/
+  long int i, j, next;
+  /*numbers used in calculation; long int is used as it can hold bigger values*/
+  /*because if we don't the statemt for i != 12586269025* we always be true*/
+  i = 1;
+  j = 2;
+  for (n = 1; n <= 98; n++)
+    {
+      if (i != 20365011074)
+	{
+	  printf("%ld, ", i);
+	}
+      else
+	{
+	  printf("%ld\n", i);
+	}
+      next = i + j;
+      i = j;
+      j = next;
+    }
+  return (0);
+}
