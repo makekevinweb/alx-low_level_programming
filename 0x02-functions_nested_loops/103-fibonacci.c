fe941293e2d6fa7ce5dd21dcc126e7e4546d1d55
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-07 18:31:52 +0000

    new file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main -  prints the sum of even valued fibonacci number
+ *
+ *Return: always 0 on sucess
+ */
+int main(void)
+{
+  int n; /* the fibonacci number to calculate*/
+  long int i, j, next, sum;
+  /*numbers used in calculation; long int is used as it can hold bigger values*/
+  /*because if we don't the statemt for i != 12586269025* we always be true*/
+  i = 1;
+  j = 2;
+  while (i <= 4000000)
+    {
+      if ((i % 2) == 0)
+	{
+	  sum = sum + i;
+	}
+      next = i + j;
+      i = j;
+      j = next;
+    }
+  printf("%ld\n", sum);
+  return (0);
+}
