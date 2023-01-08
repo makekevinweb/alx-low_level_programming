036e45f9c3bf752bcfdbeaa91966f7681d4fed14
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-08 22:40:53 +0000

    debug

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,25 @@
  */
 void print_diagonal( int n)
 {
-  int i;
-  int j;
-  for (i = 0; i < n; i++)
-    {
-      for (j = 0; j < i; j++)
-	{
-	  _putchar(' ');
-	}
-      _putchar('\\');
-      _putchar('\n');
-    }
+int i;
+int j;
+
+if ( n < 1) /*for values below 1*/
+{
+_putchar('\n');
+}
+
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
   
 }
