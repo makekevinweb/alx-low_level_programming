90808023a4ce2e63243775a106ad61831df2dfc4
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-08 20:26:34 +0000

    debug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *print_numbers - prints the first 10 natural numbers except 2 and 4
+ *print_most_numbers - prints the first 10 natural numbers except 2 and 4
  *
  *return: Always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for(i = 0; i < 10; i++)
