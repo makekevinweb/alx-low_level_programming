597e2a44f979e98c388a90f5af404940ca66180c
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-08 19:43:00 +0000

    debug

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *mul - multiplies two integers
  *@a: integer 1
@@ -10,6 +9,5 @@ int mul(int a, int b)
 {
 int mul;
 mul = a * b;/*the product of the two integers*/
-printf("%d\n", mul);
-return (0);
+return (mul);
 }
