7f37d0a80ad36443e1b8661b754ad45c5019b019
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-08 22:07:02 +0000

    new file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_line - prints a line
+ *
+ *return: Always 0 on success
+ */
+void print_line(int n)
+{
+
+int i;
+for (i = 0; i < n; n++)
+_putchar('_');
+_putchar('\n');
+}
