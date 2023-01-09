61f3276cc08f2f17f53a85852c9b9eed9c5e7e29
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-09 18:38:10 +0000

    debugg

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,35 +8,35 @@
  */
 
 int main(void)
-{ 
+{
 long int num = 612852475143;
-long int max_factor; /* this contains the biggest factor which is a prime number*/
- long int i;
- 
+long int max_factor; /*biggest factor which is a prime number*/
+long int i;
+
 /*we first divide the num by 2*/
- while (num % 2 == 0)
-   {
-     max_factor = 2;
-     num /= 2;
-   }
+while (num % 2 == 0)
+{
+max_factor = 2;
+num /= 2;
+}
 
- /**num is now an odd number, we then start dividing the number from 3,5,7, ...*/
- for(i = 3; i <= sqrt(num); i += 2)
-   /* we don't go beyond the square root of a number*/
+/**num is now an odd number, we start dividing the number from 3,5,7, ...*/
+for (i = 3; i <= sqrt(num); i += 2)
+/* we don't go beyond the square root of a number*/
 
-   {
-     while (num % i == 0)
-       {
-	 max_factor = i;
-	 num /= i;
-       }
-   }
- /*if the num is still great than 1, then it is a prime number itself*/
- if (i > 2)
-   {
-     max_factor = num;
-   }
- 
- printf("%ld\n", max_factor);
- return (num);
+{
+while (num % i == 0)
+{
+max_factor = i;
+num /= i;
+}
+}
+/*if the num is still great than 1, then it is a prime number itself*/
+if (i > 2)
+{
+max_factor = num;
+}
+
+printf("%ld\n", max_factor);
+return (0);
 }
