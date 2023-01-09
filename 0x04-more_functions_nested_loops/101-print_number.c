a7b28ea1ff8d5259b7ae194e9a5a5c64737be3f5
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-09 20:24:10 +0000

    debugg

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,23 +5,23 @@
  *
  */
 
-  void print_number(int n)
-  {
-    unsigned int n1;
+void print_number(int n)
+{
+unsigned int n1;
+if (n < 0)/* if n negative it is converted*/
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+/* this is acts like a loop function*/
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
 
-    if (n < 0)
-      {
-	n1 = -n;
-	_putchar('-');
-      } else
-      {
-	n1 = n;
-      }
-
-    if (n1 / 10)
-      {
-	print_number(n1 / 10);
-      }
-
-    _putchar((n1 % 10) + '0');
-  }
+_putchar((n1 % 10) + '0');
+}
