db352a96383dfd79e7cc4146d1edfe23ced975cb
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-10 12:05:15 +0000

    new file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_rev - prints a string in reverse
+ *
+ *@s: string to be printed
+ *
+ */
+void print_rev(char *s)
+{
+
+int i = 0;
+
+/*calculates length of string*/
+
+while (i != '\0')
+{
+i++;
+}
+
+/*prints the string*/
+while (i >= 0)
+{
+_putchar(s[i]);
+}
+
+_putchar('\0');
+_putchar('\0');
+}
