b3055522e496f6e73184365e34b3673c1a13b4c8
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-11 17:08:20 +0000

    new file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_atoi - converts a string to a digit
+ *@s: the string to convert
+ *
+ */
+
+int _atoi(char *s)
+{
+int num, sign;
+
+ while (*s = ' ') *s++;
+ if (*s = '-') sign = -1, *s++;
+ else if(*s = '+') sign = 1; *s++;
+ while (*s != '\0') num = (num * 10) + (*s - '0'), *s++;
+ return (num * sign);
+ }
