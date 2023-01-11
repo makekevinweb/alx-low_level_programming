80f58d82a65d714603a0e550c21e7809b54c0317
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-11 09:07:02 +0000

    debugg

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,14 @@
  *
  *Return: pointer to dest
  */
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
   int len, i;
   for(len = 0; src[len] != '\0'; len++)
     ;
   for(i = 0; i <= len; i++)
     {
-      *dest[i] = *src[i];
+      dest[i] = src[i];
     }
   return (dest);
 }
