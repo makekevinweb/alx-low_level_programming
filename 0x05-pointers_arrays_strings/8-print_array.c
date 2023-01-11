4331db380b9fa7fd2a950302c4b9cc4f7f2ee0e2
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-11 08:25:02 +0000

    debugg

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,11 @@
  */
 void print_array(int *a, int n)
 {
-  int i = 0;
-  while ( i < (n - 1))
-    {
-      printf("%d , ", a[i]);
-      i++;
-    }
-  printf("%d", a[i]);
+int i = 0;
+while (i < (n - 1))
+{
+printf("%d , ", a[i]);
+i++;
+}
+printf("%d\n", a[i]);
 }
