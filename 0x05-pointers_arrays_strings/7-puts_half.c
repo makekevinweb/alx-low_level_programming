738b803276b424c488d15f49502f2ff058ae6590
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-11 05:24:15 +0000

    debug

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,22 +8,22 @@
  */
 void puts_half(char *str)
 {
-  int len, n;
+int len, n;
 
-  /* calculates length of string*/
-  for(len = 0; str[len] != '\0'; len++)
-    ;
-  n = len;
-    
-  if ( len % 3 == 0)
-    {
-      n = (len - 1) / 2;
-    }
+/* calculates length of string*/
+for(len = 0; str[len] != '\0'; len++)
+;
+n = len / 2;
+ 
+if ( len % 3 == 0)
+{
+n = (len - 1) / 2;
+}
 
-  while (n < len)
-    {
-      _putchar(str[n]);
-      n++;
-    }
-  
+while (n < len)
+{
+_putchar(str[n]);
+n++;
+}
+_putchar('\n');
 }
