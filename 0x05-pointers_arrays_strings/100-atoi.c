aceb67f6618b383ed27bbfd07055fc658d66b4c1
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-11 17:44:02 +0000

    debugg

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,9 +10,25 @@ int _atoi(char *s)
 {
 int num, sign;
 
- while (*s = ' ') *s++;
- if (*s = '-') sign = -1, *s++;
- else if (*s = '+') sign = 1; *s++;
- while (*s >= '0' && *s <= '9') num = (num * 10) + (*s++ - '0');
+ while (*s = ' ')
+   *s++;
+ if (*s = '-')
+   {
+   sign = -1,
+     *s++;
+   }
+ 
+ else if (*s = '+')
+
+   {
+     sign = 1;
+     *s++;
+   }
+ 
+ while (*s >= '0' && *s <= '9')
+   {
+     num = (num * 10) + (*s++ - '0');
+   }
+ 
  return (num * sign);
  }
