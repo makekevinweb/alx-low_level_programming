ef5488e65f1ac477bb112fd39564e2d434f6e254
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-12 15:49:34 +0000

    debug

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,7 +14,7 @@ int main(void)
   int i, sum, n;
 
   sum = 0;
-
+  
   srand(time(NULL));
 
   for (i = 0; i < 100; i++)
@@ -22,6 +22,7 @@ int main(void)
       pass[i] = rand() % 78;
       sum += (pass[i] + '0');
       putchar(pass[i] + '0');
+
       if ((2772 - sum) - '0' < 78)
 	{
 	  n = 2772 - sum - '0';
@@ -30,6 +31,5 @@ int main(void)
 	  break;
 	}
     }
-
   return (0);
 }
