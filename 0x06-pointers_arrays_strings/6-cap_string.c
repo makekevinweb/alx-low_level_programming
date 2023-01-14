2be69cb7c83b2ebe9e22dd4273f5cc76380182a6
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-14 20:50:46 +0000

    debugg

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *cap_string - capitalizes each word of a string
  * @str: the string to capitalize
@@ -8,33 +7,44 @@
  */
 char *cap_string(char *str)
 {
-  int i;
-  if (*str == '\0')
-    return (str);
-  else
-    {
-  while (str[i] != '\0')
-    {
-      if (str[0] >= 'a' && str[0] <= 'z')
-	{
-	  str[0] = (str[i] - 32);
-	  i++;
-	}
-      
-      if(str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}'|| str[i] == ' ' || str[i] == '\n'|| str[i] == '\t')
-	{
-	  if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-	    {
-	      str[i + 1] = (str[i + 1] - 32);
-	      i++;
-	    }
-	  i++;
-	}
-      else
-	{
-	  i++;
-	}
-    }
-  return (str);
+int i;
+/** checks if the string is not empty*/
+if (*str == '\0')
+return (str);
+/*capitalizes the letters*/
+else
+{
+while (str[i] != '\0')
+{
+/*checks if the first letter is capitalized or not*/
+if (str[0] >= 'a' && str[0] <= 'z')
+{
+str[0] = (str[i] - 32);
+i++;
+}
+/**checks for what is separating the words*/
+if (str[i] == ',' || str[i] == ';' ||
+str[i] == '.' || str[i] == '!' ||
+str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' ||
+str[i] == '{' || str[i] == '}' ||
+str[i] == ' ' || str[i] == '\n' ||
+str[i] == '\t')
+{
+/**checks if the next character is a letter and then  capitalizes it*/
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = (str[i + 1] - 32);
+i++;
+}
+i++;
+}
+/*this runs for other instances of the function such for letters in the words*/
+else
+{
+i++;
+}
+}
+return (str);
 }
 }
