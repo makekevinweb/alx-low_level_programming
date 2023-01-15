912d449b39fdad107c9c1cddf24b68c0daf6c8df
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-01-15 19:16:58 +0000

    debugg

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -9,15 +10,15 @@
  *
  *Return: a pointer to the result
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
   
     int len1 = strlen(n1);
     int len2 = strlen(n2);
-    int len_res = (len1 > len2 ? len1 : len2) + 1; // +1 for the potential carry
-    if (len_res > size_r) return 0; // Check if buffer is large enough
+    int len_res = (len1 > len2 ? len1 : len2) + 1; /* +1 for the potential carry*/
     int carry = 0;
-    r[len_res] = '\0'; // Null terminate the result string
+    if (len_res > size_r) return 0; /* Check if buffer is large enough*/
+    r[len_res] = '\0'; /* Null terminate the result string*/
     while (len1 > 0 || len2 > 0) {
       int num1 = (len1 > 0 ? n1[--len1] - '0' : 0);
       int num2 = (len2 > 0 ? n2[--len2] - '0' : 0);
@@ -26,7 +27,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
       r[--len_res] = (sum % 10) + '0';
     }
     if (carry > 0) {
-      if (len_res == 0) return 0; // Check if buffer is large enough
+      if (len_res == 0) return 0; /* Check if buffer is large enough*/
       r[--len_res] = carry + '0';
     }
     return r + len_res;
