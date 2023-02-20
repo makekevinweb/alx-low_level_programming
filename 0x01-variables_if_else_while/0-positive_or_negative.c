2f76412c5921c298c1a7ee93506542fbac05096e
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-02-20 13:21:20 +0000

    debugg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ if (n < 0)
 {
 printf("%d is negative",  n);
 }
-else if (a == 0)
+else if (n == 0)
 {
 printf("%d is zero", n);
 }
@@ -23,4 +23,5 @@ else
 {
 printf("%d is positive", n);
 }
+return (0);
 }
