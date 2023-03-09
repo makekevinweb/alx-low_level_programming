d173ec25b429dde48528891b5f87886e14bf3564
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-09 01:42:44 +0000

    new task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/*
+ *factorial - prints the factorial number
+ *@n: the integer
+ *retun: factorial n
+ */
+int factorial(int n)
+{
+  if (n < 0)
+    {
+      return(-1);
+    }
+  else if(n == 0)
+    {
+      return(1);
+    }
+  else if(n > 0)
+    {
+      return(n * factorial(n - 1));
+    }
+}
