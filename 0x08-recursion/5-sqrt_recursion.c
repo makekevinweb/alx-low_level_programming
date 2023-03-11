66623e0151c0cc398dd6d827cd73e7652e5deaab
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-11 05:53:10 +0000

    new file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int sqrt(int num, int root);
+
+/**
+ *_sqrt_recursion - gives the square root of a number
+ *@n: the number to search the square root for
+ *Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (sqrt(1, n));
+}
+
+/**
+ *sqrt - returns the square root of n
+ *@num: number to be used while searching for root
+ *@root: the square root
+ *Return: the root
+ */
+int sqrt(int num, int root)
+{
+if (num * num == root)
+return (num)
+else
+return (sqrt(num++, root));
+}
