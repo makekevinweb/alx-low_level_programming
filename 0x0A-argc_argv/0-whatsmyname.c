352da9d752307bd89d1d0b5c4b6d61b0811c1b47
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-19 22:54:24 +0000

    debugg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,8 @@
  *main - prints name of the program
  *@a: number of argument
  *@b: arrays of arguments
- *return : always 0
  */
 void main(int a, char *b[])
 {
 	printf("%s \n", b[0]);
-	return (0);
 }
