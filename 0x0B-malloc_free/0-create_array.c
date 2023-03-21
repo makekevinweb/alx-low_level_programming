f83da250bd03c2703c23dc64df417443b6e64b76
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-21 08:22:18 +0000

    debugg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *j = (char *) malloc(sizeof(c) * size);
+	char *j = (char *) malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
