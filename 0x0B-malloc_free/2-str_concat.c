6255ca68a80714613ce7c1bb82c45685200858f1
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-21 12:08:57 +0000

    debugg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,19 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat;
 
+	if (s1 == NULL)
+	{
+		*s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		*s2 = "";
+	}
 	concat = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
-	*concat = strcat(s1, s2);
+	concat = strcat(s1, s2);
 	return (concat);
 }
