0b1597c26766bd50fa55c8329405eb87ddf4568b
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-21 12:57:26 +0000

    new file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *alloc_grid - creats a two dimensional array with all elements initialized to 0
+ *@width: width of the array
+ *@height: height of the array
+ *Return: pointer to the two dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	*p = malloc(height * sizeof(int) * width);
+	if (*p = NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			*p[i][j] = 0;
+		}
+	}
+	return (p);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 this is for malloc and free functions
 1-strdup creates a pointer to a copy of another string
 2-str_concat.c concatenates two strings together
+3-alloc-grid.c  returns a pointer to a 2 dimensional array of integers; all integers are initialized to 0
