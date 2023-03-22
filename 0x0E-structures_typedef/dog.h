dac99a9a07650229883ce3ca850a8e615a7e90db
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-22 13:08:35 +0000

    debugg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog -Dog data.
- * @name: Name of the dog
- * @age: age of the dog.
- * @owner: owner of the dog.
- * Desctiption: a structure with the name, age and owner of a dog.
+ *struct dog - Dog data.
+ *@name: Name of the dog
+ *@age: age of the dog.
+ *@owner: owner of the dog.
+ *Desctiption: a structure with the name, age and owner of a dog.
  */
 
 struct dog
@@ -14,8 +14,6 @@ struct dog
 	char *owner;
 	float age;
 };
-/**
- *following codes are added in order to compile other files in same directory
- */
+/*following codes are added in order to compile other files in same directory*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
