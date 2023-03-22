e91952aa6e4b4d89c231ff01edfe6139d2ba72ae
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-22 13:01:19 +0000

    debugg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,4 @@ struct dog
  *following codes are added in order to compile other files in same directory
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
 #endif
