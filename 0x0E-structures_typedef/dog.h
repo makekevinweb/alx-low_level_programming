7113a8066f8a088409dc220ec87b6b33abbbefd1
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-22 13:13:35 +0000

    new file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,17 @@
  *Desctiption: a structure with the name, age and owner of a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 /*following codes are added in order to compile other files in same directory*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 #endif
