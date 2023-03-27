deb7a54cbf156e0f5cc022651f37bc83e1464a21
keycodvin <ikuzwemariuskevin7@gmail.com>
2023-03-27 07:07:59 +0000

    debugg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,16 +17,28 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	p = malloc(height * sizeof(int) * width);
+	/*allocation of memory to each row*/
+	p = (int **)malloc(height * sizeof(int));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < width; i++)
 	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			/*we free each row*/
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
 		for (j = 0; j < height; j++)
 		{
-			p[i][j] = '0';
+			p[i][j] = 0;
 		}
 	}
 	return (p);
